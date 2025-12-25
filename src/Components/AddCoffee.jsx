07a3784f20dbbbbd5bd29d9a29af127b435146ac
Atikur Rahaman <atikur.rahaman.hs@gmail.com>
2025-12-25 11:08:19 +0000

    Handle add Coffee has done

--- src/Components/AddCoffee.jsx
+++ src/Components/AddCoffee.jsx
@@ -1,13 +1,19 @@
 import React from 'react';
 
 const AddCoffee = () => {
+
+    const handleAddCoffee=e=>{
+        e.preventDefault();
+
+    }
+
     return (
         <div className='p-24'>
             <div className='p-12 text-center space-y-4'>
                 <h1 className="text-6xl">Add New Coffee</h1>
                 <p>It is a long established fact that a reader will be distraceted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using Content here.</p>
             </div>
-            <form >
+            <form onSubmit={handleAddCoffee}>
                 <div className='grid grid-cols-2  gap-6'>
                     <fieldset className="fieldset bg-base-200 border-base-300 rounded-box  border p-4 w-full">
                         <label className="label">Name</label>
