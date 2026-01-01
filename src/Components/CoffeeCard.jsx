43b397d01a928b0698836396acaae24076370464
Atikur Rahaman <atikur.rahaman.hs@gmail.com>
2026-01-01 09:37:13 +0000

    Added card properly

--- src/Components/CoffeeCard.jsx
+++ src/Components/CoffeeCard.jsx
@@ -2,6 +2,9 @@ import React from 'react';
 
 const CoffeeCard = ({ coffee }) => {
     const { name, price, quantity, photo } = coffee;
+    const handleDelete = (_id) => {
+        console.log(_id);
+    }
     return (
         <div className="card card-side bg-base-100 shadow-sm">
             <figure>
@@ -18,7 +21,7 @@ const CoffeeCard = ({ coffee }) => {
                 <div className='join join-vertical space-y-2'>
                     <button className='btn'>View</button>
                     <button className='btn'>Edit</button>
-                    <button className='btn'>X</button>
+                    <button onClick={()=>handleDelete(_id)} className='btn'>X</button>
                 </div>
             </div>
         </div>
