cf8ad1fd79ac2d2a402ce367b4d29be1652ae3ce
Atikur Rahaman <atikur.rahaman.hs@gmail.com>
2026-01-13 22:44:23 +0000

    Everythiong is working

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -5,12 +5,12 @@ import CoffeeCard from './CoffeeCard';
 const Home = () => {
     const initialCoffees = useLoaderData();
     // console.log(coffees);
-    const [coffees,setCoffees] = useState(initialCoffees);
+    const [coffees, setCoffees] = useState(initialCoffees);
     return (
         <div>
             <div className='grid grid-cols-2  gap-6'>
                 {
-                    coffees.map(coffee => <CoffeeCard key={coffee._id} coffees={coffees} setCoffees={setCoffees}></CoffeeCard>)
+                    coffees.map(coffee => <CoffeeCard key={coffee._id} coffees={coffees} coffee={coffee} setCoffees={setCoffees}></CoffeeCard>)
 
                 }
             </div>
